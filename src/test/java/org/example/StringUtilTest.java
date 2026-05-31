5dfcd729c807c2d0a0176dc6dc2fe9f3662dc4c3
VZemtsov <v.zemcov@ao-esp.ru>
2026-05-31 17:28:55 +0300

    change_name_methods_and_takeaway_version_in_properties

--- src/test/java/org/example/StringUtilTest.java
+++ src/test/java/org/example/StringUtilTest.java
@@ -9,7 +9,7 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Пример из задачи: J@va the be$t!123 -> t@eb eht av$J!123")
-    void testExampleFromTask() {
+    void reverseOnlyLetters_ExampleFromTask() {
         String input = "J@va the be$t!123";
         String expected = "t@eb eht av$J!123";
         assertEquals(expected, StringUtil.reverseOnlyLetters(input));
@@ -17,7 +17,7 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Строка только из букв (обычный реверс)")
-    void testOnlyLetters() {
+    void reverseOnlyLetters_OnlyLetters() {
         assertEquals("cba", StringUtil.reverseOnlyLetters("abc"));
         assertEquals("zyx", StringUtil.reverseOnlyLetters("xyz"));
         assertEquals("Hello", StringUtil.reverseOnlyLetters("olleH"));
@@ -25,28 +25,28 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Строка только из небуквенных символов")
-    void testOnlyNonLetters() {
+    void reverseOnlyLetters_OnlyNonLetters() {
         assertEquals("123!@#", StringUtil.reverseOnlyLetters("123!@#"));
         assertEquals("   ", StringUtil.reverseOnlyLetters("   "));
     }
 
     @Test
     @DisplayName("Пустая строка и null")
-    void testEmptyOrNull() {
+    void reverseOnlyLetters_EmptyOrNull() {
         assertEquals("", StringUtil.reverseOnlyLetters(""));
         assertEquals("", StringUtil.reverseOnlyLetters(null));
     }
 
     @Test
     @DisplayName("Небуквенные символы на границах и внутри")
-    void testNonLettersAtEdges() {
+    void reverseOnlyLetters_NonLettersAtEdges() {
         assertEquals("!dcba?", StringUtil.reverseOnlyLetters("!abcd?"));
         assertEquals("1a2b3c", StringUtil.reverseOnlyLetters("1c2b3a"));
     }
 
     @Test
     @DisplayName("Смешанный регистр и Unicode буквы")
-    void testMixedCaseAndUnicode() {
+    void reverseOnlyLetters_MixedCaseAndUnicode() {
         String input = "Привет! Мир.";
         String expected = "риМтев! ирП.";
         assertEquals(expected, StringUtil.reverseOnlyLetters(input));
@@ -54,7 +54,7 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Регистр букв сохраняется при перевороте")
-    void testCaseSensitivity() {
+    void reverseOnlyLetters_CaseSensitivity() {
         assertEquals("AbCd", StringUtil.reverseOnlyLetters("dCbA"));
         assertEquals("XyZ", StringUtil.reverseOnlyLetters("ZyX"));
         assertEquals("Hello World", StringUtil.reverseOnlyLetters("dlroW olleH"));
@@ -62,13 +62,13 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Небуквенные символы в несимметричных позициях")
-    void testNonLettersAsymmetric() {
+    void reverseOnlyLetters_NonLettersAsymmetric() {
         assertEquals("a$b#c@d!", StringUtil.reverseOnlyLetters("d$c#b@a!"));
     }
 
     @Test
     @DisplayName("Длинная строка с буквами, цифрами и символами")
-    void testLongString() {
+    void reverseOnlyLetters_LongString() {
         String longInput = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=[]{}|;:,.<>?";
         String expected = "zyxwvutsrqponmlkjihgfedcba1234567890!@#$%^&*()_+-=[]{}|;:,.<>?";
         assertEquals(expected, StringUtil.reverseOnlyLetters(longInput));
@@ -76,14 +76,14 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Диакритические знаки (буквы с ударениями)")
-    void testDiacritics() {
+    void reverseOnlyLetters_Diacritics() {
         assertEquals("éèêë", StringUtil.reverseOnlyLetters("ëêèé"));
         assertEquals("éè!êë", StringUtil.reverseOnlyLetters("ëê!èé"));
     }
 
     @Test
     @DisplayName("Строка без букв (отрицательный тест)")
-    void testNoLetters() {
+    void reverseOnlyLetters_NoLetters() {
         String input = "123!@#";
         assertEquals(input, StringUtil.reverseOnlyLetters(input));
         assertEquals("   ", StringUtil.reverseOnlyLetters("   "));
@@ -92,7 +92,7 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Эмодзи и другие небуквенные Unicode символы")
-    void testEmojisAndSpecialUnicode() {
+    void reverseOnlyLetters_EmojisAndSpecialUnicode() {
         assertEquals("😊abc😊", StringUtil.reverseOnlyLetters("😊cba😊"));
         assertEquals("😊a😊b😊c", StringUtil.reverseOnlyLetters("😊c😊b😊a"));
         assertEquals("∫a∫b∫", StringUtil.reverseOnlyLetters("∫b∫a∫"));
@@ -100,7 +100,7 @@ class StringUtilTest {
 
     @Test
     @DisplayName("Сохранение позиций небуквенных символов — сложный кейс с несколькими небуквенными подряд")
-    void testMultipleNonLettersInARow() {
+    void reverseOnlyLetters_MultipleNonLettersInARow() {
         assertEquals("d!!c b? a", StringUtil.reverseOnlyLetters("a!!b c? d"));
     }
 }
